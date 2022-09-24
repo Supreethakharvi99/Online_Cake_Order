e725b894b5e6495edffe7c935901ca437722191c
kharvi <supreetha.kharvi@capgemini.com>
2022-09-24 13:17:43 +0530

    first commit

---
+++ exception/CakeIdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.onlinecakeshopping.exception;
+
+public class CakeIdNotFoundException extends RuntimeException{
+	public CakeIdNotFoundException(String message) {
+		super(message);
+	}
+
+}
